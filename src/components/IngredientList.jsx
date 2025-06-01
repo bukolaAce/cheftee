ec969349124ccdc646d7370b5bc04b819adf2901
BukolaAce <olasupobukola@gmail.com>
2025-06-01 18:51:09 +0100

    loading state and validation

--- src/components/IngredientList.jsx
+++ src/components/IngredientList.jsx
@@ -12,10 +12,10 @@ const containerVariantss = {
     opacity: 1,
     transition: { delay: 2, duration: 2 },
   },
-  exit:{
-    x:'-100vw',
-    transition:{ease:'easeInOut'}
-  }
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
 };
 const containerVariants = {
   hidden: {
@@ -27,10 +27,10 @@ const containerVariants = {
     x: 0,
     transition: { type: "spring", delay: 0.5 },
   },
-  exit:{
-    x:'-100vw',
-    transition:{ease:'easeInOut'}
-  }
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
 };
 
 const IngredientList = (prop) => {
@@ -40,10 +40,11 @@ const IngredientList = (prop) => {
     }
   }, [prop.recipe, prop.getRef]);
   const ingredientsListItems = prop.ingredients.map((ingredient) => (
-    <motion.li  variants={containerVariants}
-     initial="hidden"
-     animate="visible"
-     exit='exit'
+    <motion.li
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
       className="text-xl list-disc marker:text-xl marker:text-[#475566] text-black"
       key={ingredient}
     >
@@ -52,12 +53,14 @@ const IngredientList = (prop) => {
   ));
   return (
     <>
-      <div className="flex flex-col mb-10">
-        <motion.h1 className="mb-5 text-3xl font-bold font-title text-[#151412]"
-        variants={containerVariants}
-     initial="hidden"
-     animate="visible"
-     exit='exit'>
+      <div className="flex flex-col mb-10 ">
+        <motion.h1
+          className="mb-5 text-3xl font-bold font-title text-[#151412]"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+        >
           Ingredients on hand:
         </motion.h1>
         {/* List of Ingredients */}
@@ -69,10 +72,13 @@ const IngredientList = (prop) => {
       {prop.ingredients.length >= 4 && (
         <>
           {/* AI section   */}
-          <motion.div   variants={containerVariantss}
-     initial="hidden"
-     animate="visible"
-     exit='exit' className="bg-gray-200 p-5 rounded-lg md:w-[31rem] flex items-center justify-center h-32 w-[18rem]">
+          <motion.div
+            variants={containerVariantss}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            className="bg-gray-200 p-5 rounded-lg md:w-[31rem] flex items-center justify-center h-32 w-[18rem]"
+          >
             <div className="space-y-2" ref={prop.getRef}>
               <h1 className="font-bold md:text-lg">Ready for a recipe?</h1>
               <p className="flex pr-4">
@@ -81,12 +87,18 @@ const IngredientList = (prop) => {
               </p>
             </div>
             <div>
-              <button
-                className="text-white btn btn-active btn-error"
-                onClick={prop.getRecipe}
-              >
-                Get a Recipe
-              </button>
+              {prop.loading ? (
+                <button className="text-white btn btn-active btn-error">
+                  <span className="loading loading-spinner"></span>
+                </button>
+              ) : (
+                <button
+                  className="text-white btn btn-active btn-error"
+                  onClick={prop.getRecipe}
+                >
+                  Get a Recipe
+                </button>
+              )}
             </div>
           </motion.div>
         </>
--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 import { motion } from "framer-motion";
 import { useRef, useState } from "react";
-
+import { ToastContainer, toast } from "react-toastify";
+import { useEffect } from "react";
 import Recipe from "./Recipe";
 import IngredientList from "./IngredientList";
 import { getRecipeFromMistral } from "../Data/ai";
+
 const containerVariants = {
   hidden: {
     opacity: 0,
@@ -13,10 +15,10 @@ const containerVariants = {
     opacity: 1,
     transition: { delay: 1.5, duration: 1.5 },
   },
-  exit:{
-    x:'-100vw',
-    transition:{ease:'easeInOut'}
-  }
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
 };
 // Chef Claude
 const ChefClaudeComponent = () => {
@@ -25,17 +27,38 @@ const ChefClaudeComponent = () => {
 
   const [recipeShown, setRecipeShown] = useState(false);
   const [recipe, setRecipe] = useState("");
+  const [check, setCheck] = useState(false);
+  const [loading, setLoading] = useState(false);
   async function getRecipe() {
+    setLoading(true);
     setRecipeShown((prevShown) => !prevShown);
     const recipeMarkDown = await getRecipeFromMistral(ingredients);
     setRecipe(recipeMarkDown);
+    setLoading(false);
   }
+
+  useEffect(() => {
+    if (check) {
+      toast("Input at Least Four Ingredients");
+    }
+  }, [check]);
 
   const handleSubmit = (formData) => {
     const spice = formData.get("ingredient");
-    if (spice.length === 0) return;
-    else {
-      setIngredients((prev) => [...prev, spice]);
+
+    if (spice.length === 0) {
+      setCheck(true);
+    } else {
+      setIngredients((prev) => {
+        const updatedIngredients = [...prev, spice];
+
+        if (updatedIngredients.length >= 4) {
+          setCheck(true); // Immediately set check to true
+          setTimeout(() => setCheck(false), 5000); // Then set it to false after 3 seconds
+        }
+
+        return updatedIngredients;
+      });
     }
   };
 
@@ -45,29 +68,33 @@ const ChefClaudeComponent = () => {
     <>
       <div className="flex flex-col justify-center items-center bg-[#ffff] md:px-96 md:justify-start md:items-start">
         <motion.form
-        variants={containerVariants}
-     initial="hidden"
-     animate="visible"
-     exit='exit'
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
           action={handleSubmit}
           className="flex flex-col gap-2 mt-5 mb-2 sm:flex-row"
         >
-          <input
-            type="text"
-            placeholder="input at least 4 ingredients"
-            className="w-full max-w-xs input input-bordered focus:outline-none"
-            name="ingredient"
-          />
+          <div className="mb-5">
+            <input
+              type="text"
+              placeholder="Input at least 4 ingredients"
+              className="w-full max-w-xs input input-bordered focus:outline-none"
+              name="ingredient"
+            />
+          </div>
+
           <button
             type="submit"
             className="text-white bg-[#151412] btn btn-wide hover:bg-[#27292c] border-none"
           >
             + Add ingredient
           </button>
+          <ToastContainer
+           theme="dark"
+          />
         </motion.form>
-         
-          
-      
+
         {ingredients.length > 0 && (
           <>
             <IngredientList
@@ -75,6 +102,7 @@ const ChefClaudeComponent = () => {
               getRecipe={getRecipe}
               getRef={getRef}
               recipe={recipe}
+              loading={loading}
             />
           </>
         )}
