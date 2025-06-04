1a0bcc57819b83aa3f7ec1e00fe16d542033391b
BukolaAce <olasupobukola@gmail.com>
2025-06-04 10:43:24 +0100

    title added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 
 
 
+import {  useEffect } from "react";
 import ChefClaude from "./pages/ReactState/ChefClaude";
 
 export default function App() {
+  useEffect(()=>{
+    document.title = "Chef Tee - Your AI Cooking Assistant";
+  },[])
 
 
   return (
